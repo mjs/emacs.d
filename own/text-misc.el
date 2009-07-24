abb41e8c1db10159a3423438a0525d3de3b3c8d1
menno <none@none>
2009-07-24 13:48:41 +0100

    Whitespace handling improvements
    C-c C-w will set a hook to auto delete trailing whitespace.
    Only show trailing whitespace in Python files.

--- own/text-misc.el
+++ own/text-misc.el
@@ -5,4 +5,14 @@
       (beginning-of-buffer)
       (query-replace sym with))))
 
+
+;; Add a per-buffer hook to automatically remove trailing whitespace on write
+(defun auto-del-trailing-whitespace ()
+  (interactive)
+  (add-hook 'write-contents-hooks 'delete-trailing-whitespace t)
+  (message "Trailing whitespace will be removed on write"))
+
+(global-set-key (kbd "C-c C-w") 'auto-del-trailing-whitespace)
+
+
 (provide 'text-misc)
