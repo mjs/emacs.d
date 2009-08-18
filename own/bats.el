9e859a6860c5024cfcf5ab527269ed7f72133944
Menno Smits <menno@freshfoo.com>
2009-08-18 16:45:33 +0100

    bats-insert-org-bug and binding for magit-status

--- own/bats.el
+++ own/bats.el
@@ -1,4 +1,6 @@
 (require 'bugzilla)
+(require 'magit)
+(require 'org-config)
 
 (defun bats-insert-bug-title (bug-id)
   (interactive "NBug ID: ")
@@ -6,6 +8,14 @@
     (insert (format "Bug %s - %s"
                   (cdr (assq 'id bug))
                   (cdr (assq 'desc bug))))
+    ))
+
+(defun bats-insert-org-bug (bug-id)
+  (interactive "NBug ID: ")
+  (let ((bug (bugzilla-read-bug bug-id)))
+    (insert (format "%s [[bug:%s]]"
+                  (cdr (assq 'desc bug))
+                  (cdr (assq 'id bug))))
     ))
 
 
@@ -41,9 +51,11 @@ None
 ;; ---------------------------------------------------------
 (global-set-key (kbd "C-# C-t") 'bats-insert-bug-title)
 (global-set-key (kbd "C-# C-r") 'bats-release-notes)
+(global-set-key (kbd "C-# g") 'magit-status)
 
-
-
-
+;; BATS only bindings for org-mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            (define-key org-mode-map (kbd "C-; b") 'bats-insert-org-bug)))
 
 (provide 'bats)
