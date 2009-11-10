9faf050fe3974d12741b9c53e857872b5e0f5d98
Menno Smits <menno@freshfoo.com>
2009-11-10 09:52:48 +0000

    added Google account details

--- site/mslin.el
+++ site/mslin.el
@@ -3,5 +3,11 @@
 
 (visit-tags-table "~/source/TAGS")
 
-(setq jabber-account-list '(("msmits@batsutil")))
+(setq jabber-account-list '(("msmits@batsutil")
+                            ("menno.smits@gmail.com"
+                              (:network-server . "talk.google.com")
+                              (:port . 443)
+                              (:connection-type . ssl))
+                            ))
+
 (jabber-connect-all)
