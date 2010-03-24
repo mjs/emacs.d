ee8cb270f5ab97c7f6431b35b232badc59820cca
Menno Smits <menno@freshfoo.com>
2010-03-24 17:28:56 +0000

    update BATS connection details

--- site/mslin.el
+++ site/mslin.el
@@ -3,11 +3,9 @@
 
 (visit-tags-table "~/source/TAGS")
 
-(setq jabber-account-list '(("msmits@batsutil")
+(setq jabber-account-list '(("msmits@lxchat" (:connection-type . ssl))
                             ("menno.smits@gmail.com"
                               (:network-server . "talk.google.com")
                               (:port . 443)
                               (:connection-type . ssl))
                             ))
-
-;;(jabber-connect-all)
