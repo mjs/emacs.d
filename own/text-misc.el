50b5881be99af7d69b0ab8f4fe42f8da06585174
Menno Smits <menno@freshfoo.com>
2010-09-22 10:21:58 +0100

    added integer incrementing and decrementing

--- own/text-misc.el
+++ own/text-misc.el
@@ -6,7 +6,20 @@
            ; Using r-f-mb so that the prompt can be dynamic and the
            ; thing to be replaced can be used as an initial value
            (with (read-from-minibuffer (concat "Replace \"" sym "\" with: ") sym)))
+      (beginning-of-buffer)
       (query-replace sym with))))
+
+(defun increment-number-at-point (&optional how-much)
+  (interactive)
+  (skip-chars-backward "0123456789")
+  (or (looking-at "[0123456789]+")
+      (error "No number at point"))
+  (let ((inc-by (if how-much how-much 1)))
+    (replace-match (number-to-string (+ inc-by (string-to-number (match-string 0)))))))
+
+(defun decrement-number-at-point ()
+  (interactive)
+  (increment-number-at-point -1))
 
 ;; Add a per-buffer hook to automatically remove trailing whitespace on write
 (defun auto-del-trailing-whitespace ()
