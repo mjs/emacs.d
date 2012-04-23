ab6933b8deb89affe8e4aaada31659e869b123d5
Menno Smits <menno@freshfoo.com>
2012-04-23 21:29:31 +0100

    elemental upgrade

--- own/elemental.el
+++ own/elemental.el
@@ -18,17 +18,17 @@
 ;; literals and nested function calls are involved. The functions in
 ;; this module make this process easier.
 ;;
-;; The main functions of interest are elem-forward and elem-transpose.
+;; The main functions of interest are elem/forward and elem/transpose.
 ;;
 ;; Given a source code line, here's how the point moves with successive
-;; calls to elem-forward (the | shows the point):
+;; calls to elem/forward (the | shows the point):
 ;;
 ;;     myfunc(|123, "some, string", fn(a, b))
 ;;     myfunc(123|, "some, string", fn(a, b))
 ;;     myfunc(123, "some, string"|, fn(a, b))
 ;;     myfunc(123, "some, string", fn(a, b)|)
 ;;
-;; Similarly, here's how successive calls to elem-transpose work:
+;; Similarly, here's how successive calls to elem/transpose work:
 ;;
 ;;     myfunc(|123, "some, string", fn(a, b))
 ;;     myfunc("some, string", 123|, fn(a, b))
@@ -36,7 +36,7 @@
 ;;
 ;; These functions work correctly over multiple lines and respect
 ;; whitespace around elemnents. For example, here's how successive
-;; elem-transpose calls work on this multi-line list/array:
+;; elem/transpose calls work on this multi-line list/array:
 ;;
 ;;     [  1|23,
 ;;        "some, string",
@@ -50,14 +50,11 @@
 ;;        fn(a, b),
 ;;        123|  ]
 ;;
-;; elem-forward and elem-transpose take an optional numerical argument
+;; elem/forward and elem/transpose take an optional numerical argument
 ;; (which can be specified as a prefix argument). Positive numbers
 ;; cause the action to be applied multiple times in the forward
 ;; direction. Negative numbers cause the action to be applied
 ;; backwards.
-;;
-;; You may also like to use elem-forward-one, elem-backward-one or
-;; elem-transpose-backward for keyboard bindings.
 
 ;; Installation:
 ;;
@@ -68,9 +65,9 @@
 ;; Example config:
 ;;
 ;;   (require 'elemental)
-;;   (global-set-key (kbd "C-(") 'elem-backward-one)
-;;   (global-set-key (kbd "C-)") 'elem-forward)
-;;   (global-set-key (kbd "C-*") 'elem-transpose)
+;;   (global-set-key (kbd "C-(") 'elem/backward-one)
+;;   (global-set-key (kbd "C-)") 'elem/forward)
+;;   (global-set-key (kbd "C-*") 'elem/transpose)
 
 ;; Limitations:
 ;;
@@ -83,8 +80,8 @@
 ;; Tests:
 ;;
 ;; Unit tests can be found in elemental-tests.el. They rely on the ERT
-;; framework which is included with Emacs 24 . Older versions are also
-;; available here: https://github.com/ohler/ert
+;; framework which is included with Emacs 24. It is also available
+;; here: https://github.com/ohler/ert
 ;;
 ;; See the top of elemental-tests.el for details on running the tests.
 
@@ -106,62 +103,132 @@
 ;; 02111-1307, USA.
 
 
-(defun elem-forward-one ()
+(defun elem/forward (arg)
+  "Move over 'listish elements'
+
+  The definition of a listish element varies according to the
+  current mode but would typically include lists, arrays, tuples,
+  dictionaries and function arguments in program source code.
+
+  This function does nothing if the point is not inside a 'listish
+  structure'.
+
+  When prefix arg is positive, move that many elements forward.
+  When prefix arg is negative, move that many elements backwards.
+  An arg of 0, means the point won't move.
+  "
+  (interactive "p*")
+  (if (>= arg 0)
+      (dotimes (_ arg) (elem/forward-one))
+    (dotimes (_ (abs arg)) (elem/backward-one))))
+
+(defun elem/transpose (arg)
+  "Interchange 'listish elements' at the point.
+
+  The semantics are the same as other Emacs transpose
+  functions (e.g. transpose-words).
+
+  See elem/forward for a description of 'listish elements'.
+
+  When prefix arg is positive, transpose that many elements forward.
+  When prefix arg is negative, tranpose that many elements backwards.
+  An arg of 0, means nothing happens.
+  "
+  (interactive "*p")
+  (unless (eq arg 0)
+    (transpose-subr 'elem/forward arg)))
+
+(defun elem/forward-one ()
+  "Move forward one listish element"
   (interactive)
-  (unless (elem-outside-parens?)
-    (elem-move-out-of-string-if-required)
+  (unless (elem/outside-parens?)
+    (elem/move-out-of-string-if-required)
     (condition-case nil
         (progn
-          (forward-sexp)
-          (while (not (elem-looking-forward-ignoring-ws ","))
-            (forward-sexp)))
+          (elem/forward-sexp-skipping-comments)
+          (while (not (elem/looking-at-over-ws-and-comments ","))
+            (elem/forward-sexp-skipping-comments)))
       (scan-error nil))))
 
-(defun elem-backward-one ()
+(defun elem/backward-one ()
+  "Move backward one listish element"
   (interactive)
-  (unless (elem-outside-parens?)
+  (unless (elem/outside-parens?)
     (condition-case nil
         (progn
-          (unless (elem-move-out-of-string-if-required)
-            (backward-sexp))
-          (while (not (elem-looking-back-ignoring-ws ","))
-            (backward-sexp)))
+          (unless (elem/move-out-of-string-if-required)
+            (elem/backward-sexp-skipping-comments))
+          (while (not (elem/looking-back-over-ws-and-comments ","))
+            (elem/backward-sexp-skipping-comments)))
       (scan-error nil))))
 
-(defun elem-looking-forward-ignoring-ws (regex)
+(defun elem/forward-sexp-skipping-comments ()
+  "Like forward-sexp, but keeps going if the point end up on a comment"
+  (forward-sexp)
+  (while (elem/in-comment-by-font-lock? (point))
+    (forward-sexp)))
+
+(defun elem/backward-sexp-skipping-comments ()
+  "Like backward-sexp, but keeps going if the point end up on a comment"
+  (backward-sexp)
+  (while (elem/in-comment-by-font-lock? (point))
+    (backward-sexp)))
+
+(defun elem/looking-at-over-ws-and-comments (regex)
+  "Like looking-at, but pretends whitespace and comments aren't there"
   (save-excursion
-    (elem-skip-ws 'char-after 'forward-char)
+    (elem/skip-ws-and-comments 0 'forward-char)
     (looking-at regex)))
 
-(defun elem-looking-back-ignoring-ws (regex)
+(defun elem/looking-back-over-ws-and-comments (regex)
+  "Like looking-back, but pretends whitespace and comments aren't there"
   (save-excursion
-    (elem-skip-ws 'char-before 'backward-char)
+    (elem/skip-ws-and-comments -1 'backward-char)
     (looking-back regex)))
 
-(defun elem-skip-ws (look-func move-func)
-  (while (= (char-syntax (funcall look-func)) 32)
-    (funcall move-func)))
+(defun elem/skip-ws-and-comments (look-offset move-func)
+  (while (elem/in-ws-or-comment? (+ (point) look-offset))
+      (funcall move-func)))
 
-(defun elem-outside-parens? ()
+(defun elem/in-ws-or-comment? (where)
+  "Return non-nil if character at WHERE is whitespace or part of a comment
+
+  Use both Emacs' internal parser state tables and font-lock to work
+  this out as neither approach is quite good enough on it's own."
+  (or (elem/in-comment-or-ws-by-syntax? where)
+      (elem/in-comment-by-font-lock? where)))
+
+(defun elem/in-comment-or-ws-by-syntax? (where)
+  "Return non-nil if char at WHERE is whitespace or part of a comment,
+  using Emacs' parser state and the mode's syntax tables."
+  (memq (char-syntax (char-after where)) elem/comment-and-ws-classes))
+
+(defun elem/in-comment-by-font-lock? (where)
+  "Return non-nil if char at WHERE is part of a comment,using the
+  font-lock face property"
+  (memq (get-text-property where 'face) elem/comment-faces))
+
+(defun elem/outside-parens? ()
+  "Return non-nil if the point is outside of any parentheses
+  (as specified by the buffer's mode)."
   (<= (car (syntax-ppss)) 0))
 
-(defun elem-move-out-of-string-if-required ()
+(defun elem/move-out-of-string-if-required ()
+  "If the point is inside a source code string literal, move the point
+  to the start of the string.
+
+  Returns non-nil if this happens, nil otherwise."
   (let ((pstate (syntax-ppss)))
     (when (nth 3 pstate)
         (goto-char (nth 8 pstate)))))    ; in a string, move to start
 
-(defun elem-forward (arg)
-  (interactive "p*")
-  (if (>= arg 0)
-      (dotimes (_ arg) (elem-forward-one))
-    (dotimes (_ (abs arg)) (elem-backward-one))))
+(defconst elem/comment-faces
+  '(font-lock-comment-face
+    font-lock-comment-delimiter-face)
+  "Font lock faces used for comments")
 
-(defun elem-transpose (arg)
-  (interactive "*p")
-  (transpose-subr 'elem-forward arg))
-
-(defun elem-transpose-backward (arg)
-  (interactive "*p")
-  (transpose-subr 'elem-forward (- arg)))
+(defconst elem/comment-and-ws-classes
+  '(32 33 60 62)
+  "Emacs syntax classes for whitespace and comments")
 
 (provide 'elemental)
