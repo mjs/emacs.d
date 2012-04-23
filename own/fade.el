929b442257d85f3fe85aff61c8dc4252dd7573da
Menno Smits <menno@freshfoo.com>
2012-04-23 21:30:21 +0100

    support multiple simultaenous fades in different buffers

--- own/fade.el
+++ own/fade.el
@@ -1,6 +1,5 @@
 (provide 'fade)
 
-;; XXX add support for multiple fades at the same time by passing the overlay to fade/step
 ;; XXX control of fade interval and step
 
 (defun fade/start ()
@@ -11,22 +10,22 @@
   (if (and (boundp 'fade-overlay) (overlayp 'fade-overlay))
       (move-overlay fade-overlay (point-min) (point-max))
     (setq fade-overlay (make-overlay (point-min) (point-max))))
-  (fade/step))
+  (fade/step fade-color fade-overlay))
 
 (defun fade/clear ()
   (interactive)
   (when (overlayp fade-overlay)
     (delete-overlay fade-overlay)))
 
-(defun fade/step ()
+(defun fade/step (fade-color fade-overlay)
   (overlay-put fade-overlay 'face (list :foreground fade-color))
   (setq fade-color (fade/color-triple-to-name (mapcar 'fade/step-color-value
                                               (color-values fade-color))))
   (unless (string= fade-color "#000000")
-    (run-at-time 0.1 nil 'fade/step)))
+    (run-at-time 0.1 nil 'fade/step fade-color fade-overlay)))
 
 (defun fade/step-color-value (cval)
-  (max 0 (- cval 3000)))
+  (max 0 (- cval 4000)))
 
 (defun fade/color-triple-to-name (ctriple)
   (apply 'format "#%02x%02x%02x" (mapcar (lambda (c) (/ c 257)) ctriple)))
