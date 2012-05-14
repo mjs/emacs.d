482bf095bec19491d92ffde100a792620d529455
Menno Smits <menno@freshfoo.com>
2012-05-14 12:12:01 +0100

    change how rename-file-and-buffer gets its input
    Use read-from-minibuffer so that the current filename is used as the
    default for the new name.

--- own/file-misc.el
+++ own/file-misc.el
@@ -1,18 +1,19 @@
-;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
-(defun rename-file-and-buffer (new-name)
+;; modified from: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer ()
   "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
+  (interactive)
   (let ((name (buffer-name))
-    (filename (buffer-file-name)))
+        (filename (buffer-file-name)))
     (if (not filename)
-    (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-      (message "A buffer named '%s' already exists!" new-name)
-    (progn
-      (rename-file filename new-name 1)
-      (rename-buffer new-name)
-      (set-visited-file-name new-name)
-      (set-buffer-modified-p nil))))))
+        (message "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-from-minibuffer "Rename to: " filename)))
+        (if (get-buffer new-name)
+            (message "A buffer named '%s' already exists!" new-name)
+          (progn
+            (rename-file filename new-name t)
+            (rename-buffer new-name)
+            (set-visited-file-name new-name)
+            (set-buffer-modified-p nil)))))))
 
 
 (defun move-buffer-file (dir)
