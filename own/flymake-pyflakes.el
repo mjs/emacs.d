360ccab607aa0985f305b022d73dc218aac10f42
Menno Smits <menno@freshfoo.com>
2012-07-10 10:09:25 +0100

    allow flymake-pyflakes to work on any Python file regardless of extension

--- own/flymake-pyflakes.el
+++ own/flymake-pyflakes.el
@@ -2,13 +2,11 @@
 ;;
 ;; Author: Menno Smits
 ;;
-;; Inspired by flymake-ruby.el by Steve Purcell
-;; http://github.com/purcell/emacs.d/blob/master/site-lisp/flymake-ruby/flymake-ruby.el
+;; Inspired by flymake-ruby.el by Steve Purcell (which has since gone
+;; away). This has been heavily modified since.
 ;;
 ;; Usage:
 ;; (require 'flymake-pyflakes)
-
-(defvar flymake-pyflakes-allowed-file-name-masks '(("\\.py\\'" flymake-pyflakes-init)))
 
 (defun flymake-pyflakes-create-tempfile (filename prefix)
   (make-temp-file (or prefix "flymake-pyflakes")))
@@ -18,19 +16,18 @@
                      'flymake-pyflakes-create-tempfile)))
     (list "pyflakes" (list temp-file))))
 
-
 (defun flymake-pyflakes-load ()
   (let ((file-name (buffer-file-name (current-buffer))))
     ; Don't start flymake when a symlink is being followed as the
     ; buffer gets killed immediately leading to a "Buffer has a
     ; running process" warning.
     (when (string= file-name (file-truename file-name))
-      (set (make-local-variable 'flymake-allowed-file-name-masks) flymake-pyflakes-allowed-file-name-masks)
+      ; flymake-pyflakes-load will be called for all files that
+      ; trigger python-mode so don't be specific about the file name
+      ; mask.
+      (set (make-local-variable 'flymake-allowed-file-name-masks) '((".+" flymake-pyflakes-init)))
       (flymake-mode t))))
 
 (add-hook 'python-mode-hook 'flymake-pyflakes-load)
 
 (provide 'flymake-pyflakes)
-
-
-
