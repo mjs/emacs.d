fa8b121a46ef66ed36e7bbe78cd8e35ec4995c1c
Menno Smits <menno@freshfoo.com>
2013-03-27 08:12:23 +0000

    Python mode cleanup
    - Using python.el (hopefully from Emacs 24.3) instead of python-mode
    - Removed unused ipython.el

--- own/python-config.el
+++ own/python-config.el
@@ -5,15 +5,10 @@
                              (expand-file-name "~/.emacs.d/pylib")
                              (getenv "PYTHONPATH")))
 
+(require 'python)   ; built-in Emacs version (good as of Emacs 24.3)
 (require 'text-misc)
 (require 'flymake-pyflakes)
 (require 'python-pylint)
-
-;; python mode settings
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                   interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
 
 ;; Pymacs and ropemacs
 (require 'pymacs)
@@ -49,15 +44,11 @@
 
 (defun python-customizations ()
   "Additional customizations for python mode"
-  (define-key py-mode-map "\C-c\C-c" 'python-pylint)
-  (define-key py-mode-map "\C-cb"   'py-pdbrc-breakpoint)
-  (define-key py-mode-map "\C-cwc"  'py-which-class)
-  (define-key py-mode-map "\C-cwf"  'py-which-function))
+  (define-key python-mode-map "\C-c\C-c" 'python-pylint)
+  (define-key python-mode-map "\C-cb"   'py-pdbrc-breakpoint)
+  (define-key python-mode-map "\C-cwc"  'py-which-class)
+  (define-key python-mode-map "\C-cwf"  'py-which-function))
 
 (add-hook 'python-mode-hook 'python-customizations)
-
-;; IPython support
-(setq ipython-command "ipython")
-(require 'ipython)
 
 (provide 'python-config)
