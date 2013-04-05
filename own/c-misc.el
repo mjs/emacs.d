808b65f1163601b19c8ff14d6d05cabf3a1b430a
Menno Smits <menno@freshfoo.com>
2013-04-05 16:58:58 +0100

    Removed cc-mode that is not recent anymore (and wasn't being used anyway)

--- own/c-misc.el
+++ own/c-misc.el
@@ -1,19 +1,14 @@
 ;; Basic customisation for C/C++ and related
 
-; Use an up to date cc-mode
-(add-to-list 'load-path "~/.emacs.d/cc-mode")
-
 ; Essential for C/C++ programming
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
 
-(add-to-list 'auto-mode-alist '("\\.h$" . cc-mode))
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+
 (setq c-basic-offset 4)
 (setq c-default-style '((c++-mode . "k&r") 
                         (java-mode . "java") 
                         (awk-mode . "awk") 
                         (other . "gnu")))
-
-;(setq c-indent-comment-alist '((anchored-comment column . 0) (end-block space . 1) (cpp-end-block space . 2) (other align space . 1)))
-;(setq c-offsets-alist '((case-label . 0) (arglist-close . 0) (innamespace . 0)))
 
 (provide 'c-misc)
