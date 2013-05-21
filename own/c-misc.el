2f5662530c29dd772f6d72b07fda030286549cf7
Menno Smits <menno@freshfoo.com>
2013-05-21 12:53:14 +0100

    fix up c++-mode association wrt to .h files

--- own/c-misc.el
+++ own/c-misc.el
@@ -3,8 +3,6 @@
 ; Essential for C/C++ programming
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
 
-(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
-
 (setq c-basic-offset 4)
 (setq c-default-style '((c++-mode . "k&r") 
                         (java-mode . "java") 
@@ -14,8 +12,10 @@
 
 (defun c-customizations ()
   "Additional customizations for C like modes"
-  (define-key c-mode-base-map (kbd "<f9>") 'compile))
+  (define-key c-mode-base-map (kbd "<f9>") 'compile)
+  (add-to-list 'auto-mode-alist '("\\.h$" . c++-mode)))
+
 
 (eval-after-load 'cc-mode '(c-customizations))
-  
+
 (provide 'c-misc)
