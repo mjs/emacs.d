8d9556ef715dd726be0cb6b573ad45416a037774
Menno Smits <menno@freshfoo.com>
2013-05-31 12:04:48 +0100

    Simplied mslin config now that we're mainly editing off scomslin

--- site/mslin.el
+++ site/mslin.el
@@ -2,18 +2,12 @@
 (require 'bats-crontab)
 (require 'org-bats-config)
 
-;; XXX this should be smart and just issue a message if the tag file
-;; isn't there
-(visit-tags-table "~/source/TAGS")
-
 ;; Turn off vc-mode on this host. Doesn't work well over NFS
 (setq vc-handled-backends nil)
 
-(setq file-cache-site-directories '("~/scohome/git.0/source"
-                                    "~/puppet-trunk"
-                                    "~/Notes"))
+(setq file-cache-site-directories '("~/Notes"))
 
-;; Allow emacsclient to work from remote hosts (really just scomslin)
+;; Allow emacsclient to work from scomslin
 (setq server-host "10.64.15.188")
 (setq server-use-tcp t)
 (setq server-window 'switch-to-buffer-other-frame)  ; open in new frame
