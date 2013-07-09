082c9727698f641a086fe7bde7cd147ec3dc5f17
Menno Smits <menno@freshfoo.com>
2013-07-09 12:52:14 +0100

    server monkeying no longer required

--- site/mslin.el
+++ site/mslin.el
@@ -7,12 +7,3 @@
 
 (setq file-cache-site-directories '("~/Notes"))
 
-;; Allow emacsclient to work from scomslin
-(setq server-host "10.64.15.188")
-(setq server-use-tcp t)
-(setq server-window 'switch-to-buffer-other-frame)  ; open in new frame
-
-(defadvice server-start (after copy-server-file activate)
-  "Copy the server file to scomslin once the server has started"
-  (interactive)
-  (copy-file "~/.emacs.d/server/server" "~/scohome/tmp/server" t))
