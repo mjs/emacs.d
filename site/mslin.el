0d7e6a2129d6058a84b4a7f38d7ee833534fdb6b
Menno Smits <menno@freshfoo.com>
2013-07-16 09:51:08 +0100

    fix conflict

--- site/mslin.el
+++ site/mslin.el
@@ -5,9 +5,5 @@
 ;; Turn off vc-mode on this host. Doesn't work well over NFS
 (setq vc-handled-backends nil)
 
-<<<<<<< local
-(setq file-cache-site-directories '("~/Notes"))
-=======
 (setq file-cache-site-directories '("~/config"))
->>>>>>> other
 
