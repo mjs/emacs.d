4363d9a38a3f9f4ba9c5e1de654c75320f8d1158
Menno Smits <menno@freshfoo.com>
2014-05-03 16:29:58 -0700

    Turn off ethan-wspace mode in .go files because gofmt fixes whitespace issues anyway

--- own/go-config.el
+++ own/go-config.el
@@ -2,7 +2,9 @@
 
 (defun my-go-mode-hook ()
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq tab-width 4))
+  (setq tab-width 4)
+  (ethan-wspace-mode -1))
+
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (require 'go-flymake)
