8b4877d9c3de7fb8a1e062767cdd278ae8ccf4fb
Menno Smits <menno@freshfoo.com>
2014-05-21 08:57:44 +1200

    various useful deps - installed when pycheck was installed but worth keeping

---
+++ elpa/epl-20140405.51/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21360 12075 305157
+;;;;;;  479000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
