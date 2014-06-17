7c37e5683c4141278a8a64450ab4180d5bdb819c
Menno Smits <menno@freshfoo.com>
2014-06-17 14:50:14 +1200

    removed BATS specific stuff

--- site/mslin.el
+++
@@ -1,9 +0,0 @@
-(require 'bats-relnotes)
-(require 'bats-crontab)
-(require 'org-bats-config)
-
-;; Turn off vc-mode on this host. Doesn't work well over NFS
-(setq vc-handled-backends nil)
-
-(setq file-cache-site-directories '("~/config"))
-
