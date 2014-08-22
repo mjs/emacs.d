fdb69eeb0dfba3ceb9e737a28d1f9d4be034910d
Menno Smits <menno@freshfoo.com>
2014-08-22 15:52:24 +1200

    messing about with using compile mode with Go

--- own/canonical-config.el
+++ own/canonical-config.el
@@ -1,12 +1,28 @@
+(require 'go-config)
+
 (setq org-agenda-files (list "~/canonical/TODO.org"))
 
 (setq juju-tags-dir (expand-file-name "~/go/src/github.com/juju"))
 
 (defun rebuild-juju-tags ()
   (interactive)
-  (let ((out-file (concat (file-name-as-directory juju-tags-dir) "TAGS")))
-    (shell-command (format "ctags --recurse -e -f %s %s" out-file juju-tags-dir))))
+  (let ((tags-file (concat (file-name-as-directory juju-tags-dir) "TAGS")))
+    (shell-command (format "ctags --recurse -e -f %s %s" tags-file juju-tags-dir))
+    (visit-tags-table tags-file)))
 
 (add-hook 'midnight-hook 'rebuild-juju-tags)
+(rebuild-juju-tags)
+
+;; TODO make this non-juju specific by hunting back towards $GOPATH for the root of the repo
+(defun compile-juju ()
+  (interactive)
+  (let ((default-directory (expand-file-name "~/go/src/github.com/juju/juju")))
+    (cd default-directory)
+    (call-interactively 'compile)))
+
+(defun canonical-go-mode-hook ()
+  (define-key go-mode-map (kbd "<f9>") 'compile-juju))
+
+(add-hook 'go-mode-hook 'canonical-go-mode-hook)
 
 (provide 'canonical-config)
