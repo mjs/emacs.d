b6c29c27c242aae810970c9e9bd91931f234d2d2
Menno Smits <menno@freshfoo.com>
2014-10-30 18:00:43 +1300

    Added revert-all-buffers

--- own/file-misc.el
+++ own/file-misc.el
@@ -67,5 +67,13 @@
    (find-file new-filename)
    t))
 
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t))))
+    (message "Reverted all unmodified buffers"))
 
 (provide 'file-misc)
