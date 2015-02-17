470b499a82f185c7690ee9126859938423582458
Menno Smits <menno@freshfoo.com>
2015-02-18 10:31:54 +1300

    full Juju refresh functionality

--- own/canonical-config.el
+++ own/canonical-config.el
@@ -13,15 +13,28 @@
 (add-hook 'midnight-hook 'rebuild-juju-tags)
 (rebuild-juju-tags)
 
+
+;; This tracks the compile command used for any .go file
+(setq go-compile-command "gotest -v")
+
 ;; TODO make this non-juju specific by hunting back towards $GOPATH for the root of the repo
 (defun compile-juju ()
   (interactive)
+  (setq compile-command go-compile-command)
   (let ((default-directory (expand-file-name "~/go/src/github.com/juju/juju")))
     (cd default-directory)
-    (call-interactively 'compile)))
+    (call-interactively 'compile))
+  (setq go-compile-command compile-command))
+
+(defun refresh-juju-bits ()
+  (interactive)
+  (revert-all-buffers)
+  (rebuild-juju-tags)
+  (refresh-file-cache))
 
 (defun canonical-go-mode-hook ()
-  (define-key go-mode-map (kbd "<f9>") 'compile-juju))
+  (define-key go-mode-map (kbd "<f9>") 'compile-juju)
+  (define-key go-mode-map (kbd "<f5>") 'refresh-juju-bits))
 
 (add-hook 'go-mode-hook 'canonical-go-mode-hook)
 
