e918b185c7d13a449b2dcfcf538f5c1374410194
Menno Smits <menno@freshfoo.com>
2015-06-03 17:26:37 +1200

    Use Emacs' js-mode over js2-mode
    js2-mode doesn't seem to compile under modern Emacsen.

--- own/js-config.el
+++ own/js-config.el
@@ -1,5 +1,3 @@
-;; javascript mode from stevey
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 
 (provide 'js-config)
