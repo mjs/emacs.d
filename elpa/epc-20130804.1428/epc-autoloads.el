5ab688f3c21ae1c1bd542801d76dbeacc1fac6a0
Menno Smits <menno@freshfoo.com>
2017-03-21 10:45:55 +1300

    Mass package update

--- elpa/epc-20130804.1428/epc-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; epc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (21005
-;;;;;;  62013 192143 808000))
-
-;;;***
-
-(provide 'epc-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; epc-autoloads.el ends here
---
+++ elpa/ripgrep-20170211.857/ripgrep-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ripgrep" "20170211.857" "Front-end for ripgrep, a command line search tool" 'nil :url "https://github.com/nlamirault/ripgrep.el" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search"))
