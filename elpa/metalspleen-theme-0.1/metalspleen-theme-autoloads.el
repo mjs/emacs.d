b3e6dad0ed1fd4911df5006f87306886f2d80b36
Menno Smits <menno@freshfoo.com>
2017-08-14 17:44:18 +1200

    Intial addition of my metalspleen theme
    Heavy customized version of a "metalheart" variant from
    http://mswift42.github.io/themecreator/
    
    Needs a little more work but is looking good.

---
+++ elpa/metalspleen-theme-0.1/metalspleen-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; metalspleen-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "metalspleen-theme" "metalspleen-theme.el"
+;;;;;;  (22929 12111 273004 584000))
+;;; Generated autoloads from metalspleen-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("metalspleen-theme-pkg.el") (22929 12111
+;;;;;;  280401 435000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; metalspleen-theme-autoloads.el ends here
---
+++ elpa/metalspleen-theme-0.1/metalspleen-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "metalspleen-theme" "0.1" "Theme" '((emacs "24")))
