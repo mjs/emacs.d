a8ad4ea5ef8499166814bfa84a7650c2701cede3
Menno Finlay-Smits <inbox@menno.io>
2017-08-24 15:39:25 +1200

    Things I forgot to add from switch to company

---
+++ elpa/company-go-20170420.515/company-go-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company-go" "20170420.515" "company-mode backend for Go (using gocode)" '((company "0.8.0") (go-mode "1.0.0")) :commit "f1eef9a6ba005abb145d7b58fdd225e83a3c6a05" :keywords '("languages"))
---
+++ own/company-config.el
@@ -0,0 +1,24 @@
+(require 'company)
+
+(setq company-minimum-prefix-length 1)               ; start completing after 1 char
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-tooltip-align-annotations t)
+;(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+; Enable company-mode in all buffers
+(add-hook 'after-init-hook 'global-company-mode)
+
+;(define-key evil-normal-state-map (kbd "C-c /") 'company-complete)
+(define-key evil-insert-state-map (kbd "TAB") #'company-indent-or-complete-common)
+(define-key evil-insert-state-map (kbd "M-/") 'company-complete)
+
+(evil-declare-change-repeat 'company-complete-common)
+
+;; Abort company-mode when exiting insert mode
+(defun abort-company-on-insert-state-exit ()
+  (company-abort))
+(add-hook 'evil-insert-state-exit-hook 'abort-company-on-insert-state-exit)
+
+(provide 'company-config)
