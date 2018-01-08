34c43a2810f041250b531302d437f5480ebe5fab
Menno Finlay-Smits <inbox@menno.io>
2018-01-09 10:43:38 +1300

    Go with plain js-mode over js2-mode for now
    Simpler and less noisy (flycheck with eslint is enough)

--- own/js-config.el
+++ own/js-config.el
@@ -1,13 +1,19 @@
-(require js2-mode)
+; (require js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 
-(require 'js2-refactor)
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
-(js2r-add-keybindings-with-prefix "C-c C-r")
-(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+(add-hook 'js-mode-hook 'flycheck-mode)
+
+;
+;(require 'js2-refactor)
+;(add-hook 'js2-mode-hook #'js2-refactor-mode)
+;(js2r-add-keybindings-with-prefix "C-c C-r")
+;(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
 
 ; TODO xref-js2 - needs Emacs 25
 ; TODO company-tern
+
+
+
 
 (provide 'js-config)
