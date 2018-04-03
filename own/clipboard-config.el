5196986cda472e7aa2eedda0704ef468b022ad0d
Menno Finlay-Smits <inbox@menno.io>
2018-04-04 09:54:17 +1200

    C-c c without region now copies buffer to clipboard

--- own/clipboard-config.el
+++ own/clipboard-config.el
@@ -3,8 +3,15 @@
 (setq x-select-enable-primary t)
 (setq mouse-drag-copy-region t)
 
+(defun clipboard-copy ()
+  "Copy the region to the clipboard.  If no region is selected, copy the buffer."
+  (interactive)
+  (if (use-region-p)
+      (clipboard-kill-ring-save (region-beginning) (region-end))
+    (clipboard-kill-ring-save (point-min) (point-max))))
+
 ;; Bindings for clipboard interaction
-(global-set-key "\C-cc" 'clipboard-kill-ring-save)
+(global-set-key "\C-cc" 'clipboard-copy)
 (global-set-key "\C-cx" 'clipboard-kill-region)
 (global-set-key "\C-cv" 'clipboard-yank)
 
