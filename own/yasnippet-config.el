48d18555ca8dd520e4b1fa758b46a2a60c5ad803
Menno Finlay-Smits <inbox@menno.io>
2018-09-11 15:19:18 +1200

    Big yas-snippet clean up

--- own/yasnippet-config.el
+++ own/yasnippet-config.el
@@ -1,6 +1,19 @@
+;;; yasnippet-config.el --- Configuration for yas-snippet
+;;
+;;; Commentary:
+;;
+;; Basic setup for yas-snippet.
+;;
+;;; Code:
+
+(require 'yasnippet)
+(require 'evil)
 
 ;; yasnippet
 (yas-global-mode 1)
-(setf yas/indent-line nil)  ; prevent annoying auto-indent behaviour
+(setf yas-indent-line nil)  ; prevent annoying auto-indent behaviour
+
+(define-key evil-insert-state-map (kbd "C-c y") 'yas-expand)
 
 (provide 'yasnippet-config)
+;;; yasnippet-config.el ends here
