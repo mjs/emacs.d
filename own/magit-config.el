8bdf3b254f870122589522598cfe19e9c56b18a6
Menno Finlay-Smits <inbox@menno.io>
2018-09-25 14:51:55 +1200

    Add header and footer

--- own/magit-config.el
+++ own/magit-config.el
@@ -1,6 +1,16 @@
+;;; magit-config.el --- Configuration for magit and friends
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+
+(require 'magit)
 (require 'evil-magit)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x C-g") 'magit-status) ; easier to type
 
 (provide 'magit-config)
+
+;;; magit-config.el ends here
