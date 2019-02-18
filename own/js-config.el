188e56cf4056c1c8bbbe31ff78afb0bf608a4b27
Menno Finlay-Smits <inbox@menno.io>
2019-02-19 10:11:39 +1300

    HTML cleanup binding
    Mainly for cacophony blog articles.

--- own/js-config.el
+++ own/js-config.el
@@ -6,6 +6,14 @@
 (eval-after-load 'js
   '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
 
+
+(defun html-init-beautify ()
+  (interactive)
+  (html-mode)
+  (web-beautify-html))
+
+(global-set-key (kbd "<f6>") 'html-init-beautify)
+
 ; TODO xref-js2 - needs Emacs 25
 ; TODO company-tern
 
