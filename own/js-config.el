4b433a9ca8ee4c1a823b4e6871d12d49b008cb51
Menno Finlay-Smits <inbox@menno.io>
2019-09-24 12:25:19 +1200

    Prettier setup

--- own/js-config.el
+++ own/js-config.el
@@ -1,12 +1,18 @@
 (require 'js)
+(require 'prettier-js)
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+
 (setq js-indent-level 2)
 
 (require 'web-beautify)
 (eval-after-load 'js
   '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
 
-(require 'prettier-js)
+(setenv "PATH" (format "%s:%s"
+                       "/opt/node-v8.16.1-linux-x64/bin"
+                       (getenv "PATH")))
+
 
 (defun html-init-beautify ()
   (interactive)
@@ -14,6 +20,7 @@
   (web-beautify-html))
 
 (global-set-key (kbd "<f6>") 'html-init-beautify)
+
 
 ; TODO xref-js2 - needs Emacs 25
 ; TODO company-tern
