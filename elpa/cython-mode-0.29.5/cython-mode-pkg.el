315b4be9ea0a21963deba7069c279f478b10b826
Menno Finlay-Smits <inbox@menno.io>
2019-09-24 12:23:35 +1200

    removed obsolete packages

--- elpa/cython-mode-0.29.5/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cython-mode" "0.29.5" "Major mode for editing Cython files" 'nil :commit "5ea22893679918a3e0c36bb14c0c20e008660bb0")
--- elpa/dash-2.14.1/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (23447 994 313394 840000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
