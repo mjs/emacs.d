00cf80deb3fa4bfc5c0c0034f7e4f1f6b3f5f47a
Menno Finlay-Smits <inbox@menno.io>
2019-12-13 11:05:52 +1300

    Include Go bin directory in exec-path (important for lsp-mode)
    Also removed unnecessary requires and added comments.

--- own/go-config.el
+++ own/go-config.el
@@ -1,29 +1,38 @@
+;;; go-config.el --- Customisations for Go programming
+;;
+;;; Commentary:
+;;
+;;
+;;; Code:
+
+; Ensure $GOPATH and $PATH are set up. exec-path is important for lsp-mode.
+(let* ((go-path (expand-file-name "~/go"))
+       (go-bin (concat go-path "/bin")))
+  (setenv "GOPATH" go-path)
+  (setenv "PATH" (format "%s:%s" go-bin (getenv "PATH")))
+  (setq exec-path (cons go-bin exec-path)))
+
+(setenv "GO111MODULE" "on")
+
 (require 'go-mode)
-(require 'lsp-config)
-(require 'company)
-(require 'company-lsp)
-
-(setenv "PATH" (format "%s:%s"
-                       (expand-file-name "~/go/bin")
-                       (getenv "PATH")))
-(setenv "GOPATH" (expand-file-name "~/go"))
-
+(require 'lsp)
+(require 'ethan-wspace)
 
 ; Use goimports instead of go-fmt
 (setq gofmt-command "my-go-fmt")
 
-(defun my-go-mode-hook ()
+(defun go-mode-customisations ()
+  "Set up go-mode to suit my tastes."
   (lsp-deferred)
 
   (if (not (string-match "go" compile-command))
-      (set (make-local-variable 'compile-command)
-           "go test "))
+      (set (make-local-variable 'compile-command) "go test "))
 
   (setq tab-width 4)
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (ethan-wspace-mode -1))
+  (ethan-wspace-mode -1)
+  (add-hook 'before-save-hook 'gofmt-before-save))
 
-(add-hook 'go-mode-hook 'my-go-mode-hook)
+(add-hook 'go-mode-hook 'go-mode-customisations)
 
 (evil-define-key 'normal go-mode-map [f9] 'compile)
 
