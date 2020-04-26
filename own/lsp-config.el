fab0b31edfeb9208ee5a71bc999689044b88d9e1
Menno Finlay-Smits <inbox@menno.io>
2020-04-26 22:32:04 +1200

    added LSP rename binding

--- own/lsp-config.el
+++ own/lsp-config.el
@@ -14,6 +14,7 @@
 (evil-global-set-key 'normal (kbd "M-.") 'lsp-find-definition)
 (evil-global-set-key 'normal (kbd "M-?") 'lsp-find-references)
 (evil-global-set-key 'normal (kbd "M-,") 'pop-tag-mark)
+(evil-global-set-key 'normal (kbd "M-l r") 'lsp-rename)
 
 (provide 'lsp-config)
 ;;; lsp-config.el ends here
