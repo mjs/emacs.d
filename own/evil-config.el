96353c51673d712f546403dbeb37f8822c2a738f
Menno Finlay-Smits <inbox@menno.io>
2020-04-26 23:59:29 +1200

    Added use-package, evil config done

--- own/evil-config.el
+++ own/evil-config.el
@@ -1,60 +1,67 @@
-(setq evil-esc-delay 0.001)           ; avoid ESC/meta mixups
+(use-package evil
+  :init
 
-(keyboard-translate ?\C-i ?\H-i)      ; map C-i to Hyper-i to avoid conflict with TAB
+  (setq evil-esc-delay 0.001)           ; avoid ESC/meta mixups
+  (keyboard-translate ?\C-i ?\H-i)      ; map C-i to Hyper-i to avoid conflict with TAB
+  (setq evil-want-C-i-jump nil)         ; don't let Evil set the binding, we'll do this ourselves
+  (setq evil-want-C-u-scroll t)
+  (setq evil-shift-width 4)
+  (setq evil-search-module 'evil-search)
 
-(setq evil-want-C-i-jump nil)         ; don't let Evil set the binding, we'll do this ourselves
-(setq evil-want-C-u-scroll t)
-(setq evil-shift-width 4)
-(setq evil-search-module 'evil-search)
+  :config
 
-(require 'evil)
+  ;; Use hippie-expand instead of evil's own completion for C-n
+  ; XXX should this be company?
+  (setq evil-complete-next-func 'hippie-expand)
+
+  ;; A;; Not sure why this is necessary but various insert mode keys don't work
+  ;; without it.
+  (evil-update-insert-state-bindings nil nil t)
+
+  :bind (
+         :map evil-normal-state-map
+         ("H-i" . evil-jump-forward)   ; Now set up C-i / H-i
+         ("S" . replace-symbol-at-point)
+
+         ("C-]" . etags-select-ultimate-find-tag)
+         ("M-]" . etags-select-find-tag)
+
+         ("=" . evil-numbers/inc-at-pt)
+         ("-" . evil-numbers/dec-at-pt)
+
+         (")" . elem/forward-one)
+         ("(" . elem/backward-one)
+         ("gs" . elem/transpose)
+         ("gS" . (lambda ()
+                  (interactive)
+                  (elem/transpose -1)))
+
+         ;; Bind this elsewhere since Evil uses C-u (think p == 'prefix')
+         ("C-p" . universal-argument)
+
+         :map evil-insert-state-map
+         ("C-p" . universal-argument)
+
+         :map evil-visual-state-map
+         ("C-p" . universal-argument)
+
+         )
+)
+
 (evil-mode 1)
 
-;; Use hippie-expand instead of evil's own completion for C-n
-(setq evil-complete-next-func 'hippie-expand)
-
-(define-key evil-normal-state-map (kbd "H-i") 'evil-jump-forward)   ; Now set up C-i / H-i
-
-;; Bind this elsewhere since Evil uses C-u (think p == 'prefix')
-(define-key evil-normal-state-map (kbd "C-p") 'universal-argument)
-(define-key evil-insert-state-map (kbd "C-p") 'universal-argument)
-(define-key evil-visual-state-map (kbd "C-p") 'universal-argument)
+(use-package evil-numbers)
+(use-package elemental)
 
 ;; Provide a menu of tags when there's multiple matches
-(require 'etags-select)
-(defun etags-select-ultimate-find-tag ()
-  "Try to find the tag at point but if there isn't one then prompt"
-  (interactive)
-  (let ((tag (find-tag-default)))
-    (if tag
-        (etags-select-find tag)
-      (etags-select-find-tag))))
-
-;; Alternate escape to normal mode
-(define-key evil-insert-state-map (kbd "C-k") 'evil-force-normal-state)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-force-normal-state)
-
-;; Not sure why this is necessary but various insert mode keys don't work
-;; without it.
-(evil-update-insert-state-bindings nil nil t)
-
-;; Useful bindings
-(define-key evil-normal-state-map (kbd "C-]") 'etags-select-ultimate-find-tag)
-(define-key evil-normal-state-map (kbd "M-]") 'etags-select-find-tag)
-
-(require 'text-misc)
-(define-key evil-normal-state-map "S" 'replace-symbol-at-point)
-
-(require 'evil-numbers)
-(define-key evil-normal-state-map "=" 'evil-numbers/inc-at-pt)
-(define-key evil-normal-state-map "-" 'evil-numbers/dec-at-pt)
-
-(require 'elemental)
-(define-key evil-normal-state-map ")" 'elem/forward-one)
-(define-key evil-normal-state-map "(" 'elem/backward-one)
-(define-key evil-normal-state-map "gs" 'elem/transpose)
-(define-key evil-normal-state-map "gS" '(lambda ()
-                                          (interactive)
-                                          (elem/transpose -1)))
+(use-package etags-select
+  :config
+  (defun etags-select-ultimate-find-tag ()
+    "Try to find the tag at point but if there isn't one then prompt"
+    (interactive)
+    (let ((tag (find-tag-default)))
+      (if tag
+          (etags-select-find tag)
+        (etags-select-find-tag)))))
 
 (provide 'evil-config)
--- own/text-config.el
+++ own/text-config.el
@@ -9,12 +9,20 @@
 ;;; Code:
 
 
-
-(require 'text-misc)
-(require 'longlines)
-
-(defalias 'll 'longlines-mode)
+(global-set-key (kbd "M-Q") 'unfill-paragraph)
 (defalias 'tt 'toggle-truncate-lines)
+
+(use-package longlines
+  :config
+  (defalias 'll 'longlines-mode))
+
+
+(use-package ethan-wspace
+  :init
+  (setq ethan-wspace-face '(t (:background "#05ff00")))
+  (setq ethan-wspace-face-customized t))
+
+(global-ethan-wspace-mode 1)
 
 (put 'narrow-to-region 'disabled nil)
 
@@ -23,14 +31,6 @@
 (put 'upcase-region 'disabled nil)
 ;; tabs are for babies and Aztecs (and Makefiles, I guess)
 (setq-default indent-tabs-mode nil)
-
-;; Get OCD about whitespace
-(setq ethan-wspace-face '(t (:background "#05ff00")))
-(setq ethan-wspace-face-customized t)
-(require 'ethan-wspace)
-(global-ethan-wspace-mode 1)
-
-(global-set-key (kbd "M-Q") 'unfill-paragraph)
 
 (provide 'text-config)
 ;;; text-config.el ends here
