213d0b158009d29311e87ea1543af32dae69b00e
Menno Finlay-Smits <inbox@menno.io>
2020-04-29 22:56:45 +1200

    Clean up go-config

--- own/go-config.el
+++ own/go-config.el
@@ -8,22 +8,16 @@
 
 (use-package go-mode
   :straight t
-  :config
-  (setq gofmt-command "gofmt")
-
-  (defun go-mode-customisations ()
-    "Set up go-mode to suit my tastes."
+  :init
+  (defun init-go-mode ()
+    (interactive)
     (lsp-deferred)
-    (if (not (string-match "go" compile-command))
-        (set (make-local-variable 'compile-command) "go test "))
     (setq tab-width 4)
-    (ethan-wspace-mode -1)
-    (add-hook 'before-save-hook 'gofmt-before-save))
-
-  ; XXX
-  (add-hook 'go-mode-hook 'go-mode-customisations)
-
-  ; XXX
-  (evil-define-key 'normal go-mode-map [f9] 'compile))
+    (ethan-wspace-mode -1))
+  :hook (go-mode . init-go-mode)
+  :hook (before-save . gofmt-before-save)
+  :config (setq gofmt-command "gofmt")
+  :bind (:map go-mode-map
+         ("<f9>" . compile)))
 
 (provide 'go-config)
