570434a466cf0ae5c38686674d1379289ca152da
Menno Finlay-Smits <inbox@menno.io>
2020-04-29 23:53:34 +1200

    Deal with keymap precence ordering issues in rust-config

--- own/rust-config.el
+++ own/rust-config.el
@@ -11,10 +11,10 @@
   :hook (rust-mode . racer-mode)
   :hook (racer-mode . eldoc-mode)
   :hook (racer-mode . company-mode)
-  :bind (:map rust-mode-map
-         ("M-." . racer-find-definition)
-         ("M-," . pop-tag-mark)
-         ("C-c C-d" . 'racer-describe)))
+  :config
+  (evil-define-key 'normal rust-mode-map (kbd "M-.") 'racer-find-definition)
+  (evil-define-key 'normal rust-mode-map (kbd "M-,") 'pop-tag-mark)
+  (evil-define-key 'normal rust-mode-map (kbd "C-c C-d") 'racer-describe))
 
 (use-package flycheck-rust
   :straight t
