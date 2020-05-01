ce34ef81dfef77654ba2542cd254f96a2aea8c22
Menno Finlay-Smits <inbox@menno.io>
2020-05-01 14:07:47 +1200

    Ensure projectile is actually loaded

--- own/projectile-config.el
+++ own/projectile-config.el
@@ -1,11 +1,11 @@
 (use-package projectile
   :straight t
-  :bind (:map projectile-mode-map
-         ("C-c p" . projectile-command-map))
-
   :config
   (setq projectile-completion-system 'default)  ; use selectrum
-  (projectile-mode +1))
+  :bind (:map projectile-mode-map
+         ("C-c p" . projectile-command-map)))
+
+(projectile-mode +1)
 
 (use-package projectile-ripgrep
   :straight t
