23644b369e9eb79c46457da8c6c6b7b62ee13a06
Menno Finlay-Smits <inbox@menno.io>
2020-05-08 00:26:10 +1200

    Avoid lower case company completions from company-dabbrev

--- own/company-config.el
+++ own/company-config.el
@@ -1,11 +1,14 @@
 (use-package company
   :straight t
   :config
-  (setq company-minimum-prefix-length 1)    ; start completing after 1 char
-  (setq company-idle-delay .3)              ; decrease delay before autocompletion popup shows
-  (setq company-tooltip-limit 20)           ; bigger popup window
+  (setq company-minimum-prefix-length 1)      ; start completing after 1 char
+  (setq company-idle-delay .3)                ; decrease delay before autocompletion popup shows
+  (setq company-tooltip-limit 20)             ; bigger popup window
   (setq company-echo-delay 0)                 ; remove annoying blinking
   (setq company-tooltip-align-annotations t)
+
+  ; Prevent occasional wrong case completions
+  (setq company-dabbrev-downcase nil)
 
   (evil-declare-change-repeat 'company-complete-common)
 
